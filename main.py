929d1bc897ac373a98151607be5669189dbbef92
AidarShamOne <aidar_shamsutdinov92@mail.ru>
2024-04-30 18:45:04 +0500

    Решения 11 задач на Hexlet

---
+++ main.py
@@ -0,0 +1,61 @@
+# print('Hello World!')
+#
+# print('')
+# print('')
+# print('')
+
+# 2.Задание на арифметику
+
+# print(3 ** 5)
+# print(-8 / -4)
+# print(100 % 3)
+# print((3 ** 5) + (-8 / -4) + (100 % 3))
+
+# 3.Задание ваша программа должна использовать только один print()
+
+# print("- Did Joffrey agree?\n- He did. He also said \"I love using \\n\".")
+
+# 4. Переменные
+
+# euro_count = 100
+# euro_per_dollar = euro_count * 1.25
+# print(euro_per_dollar)
+# dollar_per_rubles = euro_per_dollar * 60
+# print(dollar_per_rubles)
+
+# 5. магические числа
+
+# king = 'King Balon the 6th'
+# generation_of_kings = 6
+# rooms_in_castle = 17
+# print(king + ' has ' + str(generation_of_kings * rooms_in_castle) + 'rooms.')
+
+# 6. Две строки
+
+# stark = 'Arya'
+#
+# print(f"Do you want to eat, {stark}? \nYes, I'm hungry, mom.")
+
+# 7. Три переменные с фамилиями
+
+# one = 'Naharis'
+# two = 'Mormont'
+# three = 'Sand'
+#
+# # BEGIN (write your solution here)
+# print(f"{one[2]}{two[1]}{three[3]}{two[4]}{two[2]}")
+# # END
+
+# 8. Срезы
+# value = 'Hexlet'
+# print(value[2:-1])
+
+# 9.Вывод двух строк
+# print(str(2) + 'times')
+
+# 10. Строковое представление числа
+# value = "-42"
+# value = abs(int(value))
+# print(value)
+
+# 11. Две переменные
